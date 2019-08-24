6ebf8e861723392260b18d79543cf8fad274d585
kodehat <dev@codehat.de>
2019-08-24 14:43:35 +0200

    Fix app not starting on Android 9

--- generate_keystore.sh
+++ generate_keystore.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+BOUNCYCASTLE_PROVIDER_VERSION=162
+BOUNCYCASTLE_PROVIDER_FILE=bcprov-jdk15on-${BOUNCYCASTLE_PROVIDER_VERSION}.jar
+BOUNCYCASTLE_PROVIDER_URL=https://downloads.bouncycastle.org/java/${BOUNCYCASTLE_PROVIDER_FILE}
+
+if [[ ! -f ${BOUNCYCASTLE_PROVIDER_FILE} ]]; then
+    echo "Bouncycastle provider missing. Downloading from ${BOUNCYCASTLE_PROVIDER_URL}"
+    curl ${BOUNCYCASTLE_PROVIDER_URL} -o ${BOUNCYCASTLE_PROVIDER_FILE}
+    echo "Download finished."
+fi
+
 keytool -genkey -keyalg RSA \
 	-alias selfsigned \
 	-keystore keystore.jks \
@@ -10,5 +20,5 @@ keytool -genkey -keyalg RSA \
 	-validity 9999 \
 	-storetype BKS \
 	-provider org.bouncycastle.jce.provider.BouncyCastleProvider \
-	-providerpath bcprov-jdk15on-160.jar
+	-providerpath ${BOUNCYCASTLE_PROVIDER_FILE}
 	
